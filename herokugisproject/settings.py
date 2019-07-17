84c99da016d7fb41cbbc3e19d91cc833edc7948b
Grace-Amondi <ochieng.grace@students.jkuat.ac.ke>
2019-07-17 15:06:14 +0300

    first deploy

--- herokugisproject/settings.py
+++ herokugisproject/settings.py
@@ -22,7 +22,6 @@ load_dotenv(dotenv_path)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
